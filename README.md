05504b8daba3f3e813f59ab2f64c124bee073128
Kostiantyn Ponomarenko <kp@koponomarenko.com>
2021-10-31 13:45:48 +0200

    make it work - use boost via conan

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # conan-cxx-package-manager-sample
+
+Links:
+- https://conan.io/center
+- https://docs.conan.io/en/latest/cheatsheet.html
+- https://docs.conan.io/en/latest/getting_started.html
+
+
+Profiles:
+```
+$ conan profile list
+$ conan profile new ~/.conan/profiles/my-default --detect
+```
+
+Setup Conan remotes
+```
+$ conan remote list
+$ conan remote add conancenter https://center.conan.io
+```
+
+Get dependencies and generate build system files
+```
+$ mkdir cmake-build-debug && cd cmake-build-debug
+$ conan install .. -pr my-default -r conancenter -b missing
+```
--- main.cpp
+++ main.cpp
@@ -1,6 +1,38 @@
 #include <iostream>
+#include <string>
+
+#include "boost/optional.hpp"
+
+boost::optional<int> convert(const std::string& text)
+{
+    if (text == "1") {
+        return 1;
+    }
+    else {
+        return boost::none;
+    }
+}
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
+
+    const std::string s1{"1"};
+    auto value1 = convert(s1);
+    if (value1) {
+        std::cout << "s1 value is " << value1.get() << std::endl;
+    }
+    else {
+        std::cout << "can't convert s1" << std::endl;
+    }
+
+    const std::string s2{"2"};
+    auto value2 = convert(s2);
+    if (value2) {
+        std::cout << "s2 value is " << value2.get() << std::endl;
+    }
+    else {
+        std::cout << "can't convert s2" << std::endl;
+    }
+
     return 0;
 }
